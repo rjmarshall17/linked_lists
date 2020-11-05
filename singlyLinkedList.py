59b9247863ae39a679aaeabd3448e42e7e561575
Rob Marshall <rob.marshall17@gmail.com>
2020-11-05 15:59:17 -0500

    Fixes only go through list once

--- singlyLinkedList.py
+++ singlyLinkedList.py
@@ -25,7 +25,7 @@ class SinglyLinkedList:
             lastNode.next = node
         self.size += 1
     
-    def insert(self,node,position=0):
+    def insert(self,node,position=1):
         if not isinstance(node,Node):
             raise ValueError("insert: Node required but not found")
         
@@ -44,7 +44,43 @@ class SinglyLinkedList:
             node.next = current.next
             current.next = node
         self.size += 1
-        
+    
+    def remove(self,value=None):
+        if value is None:
+            raise ValueError("A value is required")
+        if self.size <= 0:
+            return True
+        node = self.head
+        prev = None
+        while node is not None:
+            if node.value == value:
+                if prev is None:
+                    self.head = self.head.next
+                else:
+                    prev.next = node.next
+                self.size -= 1
+            prev = node
+            node = node.next
+
+    def remove_nth_node_from_end(self,n):
+        s = self.head
+        f = self.head
+        c = 1
+        while c <= n:
+            c += 1
+            s = s.next
+
+        if s is None:
+            self.head.value = self.head.next.value
+            self.head.next = self.head.next.next
+            
+        else:
+            while s.next is not None:
+                s = s.next
+                f = f.next
+            f.next = f.next.next
+        self.size -= 1
+
     def __find_at_position__(self,position=-1):
         if position < 0:
             position = self.size
@@ -63,6 +99,7 @@ class SinglyLinkedList:
         ret = []
         node = self.head
         while node is not None:
+            # print("getList: Current value: %s" % node.value)
             ret.append(node.value)
             node = node.next
         return ret
@@ -71,23 +108,65 @@ class SinglyLinkedList:
         return self.size
 
 if __name__ == '__main__':
+    zero = Node("0")
     one = Node("1")
     two = Node("2")
     three = Node("3")
+    three2 = Node("3")
+    three3 = Node("3")
     four = Node("4")
     five = Node("5")
     six = Node("6")
     seven = Node("7")
+    eight = Node("8")
+    nine = Node("9")
+    ten = Node("10")
+    
+    # linkedList = SinglyLinkedList()
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # linkedList.append(one)
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # linkedList.remove("1")
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # linkedList.append(one)
+    # linkedList.append(two)
+    # linkedList.append(three)
+    # linkedList.append(four)
+    # linkedList.append(three2)
+    # linkedList.append(five)
+    # linkedList.append(three3)
+    # linkedList.append(six)
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # linkedList.remove("3")
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # print("="*20)
+
+    # linkedList = SinglyLinkedList()
+    # linkedList.append(one)
+    # linkedList.append(two)
+    # linkedList.append(three)
+    # linkedList.append(four)
+    # linkedList.append(five)
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # linkedList.insert(six,3)
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # linkedList.insert(seven,1)
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
+    # linkedList.insert(eight,5)
+    # print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
     
     linkedList = SinglyLinkedList()
+    linkedList.append(zero)
     linkedList.append(one)
     linkedList.append(two)
     linkedList.append(three)
     linkedList.append(four)
     linkedList.append(five)
+    linkedList.append(six)
+    linkedList.append(seven)
+    linkedList.append(eight)
+    linkedList.append(nine)
     print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
-    linkedList.insert(six,3)
-    print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
-    linkedList.insert(seven,1)
-    print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))
-    
+    linkedList.remove_nth_node_from_end(4)
+    print("The number of elements in the list is: %d and they are: %s" % (len(linkedList),linkedList.getList()))  
+        
